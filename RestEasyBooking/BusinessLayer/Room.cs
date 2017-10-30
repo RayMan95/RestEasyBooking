3b4f438a171dc69f8dd2b2f991a1717ed34d73c5
RayMan95 <rfakier2002@gmail.com>
2017-10-30 18:11:47 +0200

    Added room entity

--- RestEasyBooking/BusinessLayer/Room.cs
+++ RestEasyBooking/BusinessLayer/Room.cs
@@ -8,5 +8,43 @@ namespace RestEasyBooking.BusinessLayer
 {
     class Room
     {
+        public enum Season
+        {
+            Low = 0,
+            Mid = 1,
+            High = 2
+        }
+
+        private bool occupied;
+        private Season _season;
+
+        #region Properties
+        public Season season
+        {
+            get { return _season; }
+            set { _season = value; }
+        }
+        #endregion
+
+        public Room()
+        {
+            occupied = false;
+        }
+
+        public int GetPrice()
+        {
+            if (_season == Season.Low)
+            {
+                return 550;
+            }
+            else if (_season == Season.Mid)
+            {
+                return 750;
+            }
+            else
+            {
+                return 995;
+            }
+        }
     }
 }
