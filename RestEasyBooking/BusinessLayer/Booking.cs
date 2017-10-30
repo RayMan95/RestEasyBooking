7dab1f1e3a53574a3a74ee162c93a85d48228aff
RayMan95 <rfakier2002@gmail.com>
2017-10-30 18:31:10 +0200

    Added Booking entity

---
+++ RestEasyBooking/BusinessLayer/Booking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestEasyBooking.BusinessLayer
+{
+    class Booking
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+        private string referenceNumber;
+        private int roomID;
+        private long guestAccountNumber;
+
+        #region Properties
+        public DateTime StartDate
+        {
+            get { return startDate; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+        }
+
+        public int RoomNumber
+        {
+            get { return roomID + 1; }
+        }
+
+        public string ReferenceNumber
+        {
+            get { return referenceNumber; }
+        }
+
+        public long GuestAccountNumber
+        {
+            get { return guestAccountNumber; }
+            set { guestAccountNumber = value; }
+        }
+        #endregion
+
+        public Booking(DateTime start, DateTime end, int room_id, long guest_acc_num)
+        {
+            startDate = start;
+            endDate = end;
+            roomID = room_id;
+            guestAccountNumber = guest_acc_num;
+        }
+    }
+}
