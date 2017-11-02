8669ee63e38cf0709e0320868760f18ed8d0d32c
RayMan95 <rfakier2002@gmail.com>
2017-11-02 20:54:16 +0200

    Added DB base class with core DB access functionality, as well as blank GuestDB and BookingDB classes

---
+++ RestEasyBooking/DatabaseLayer/DB.cs
@@ -0,0 +1,87 @@
+﻿using RestEasyBooking.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestEasyBooking.DatabaseLayer
+{
+    class DB
+    {
+        private string strConn = Settings.Default.RestEasyBookingConnectionString;
+        protected SqlConnection cnMain;
+        protected DataSet dsMain;
+        protected SqlDataAdapter daMain;
+
+        protected string aSQLstring;  // to be initialised later
+        public enum DBOperation
+        {
+            Add = 0,
+            Edit = 1,
+            Delete = 2
+        }
+
+
+        public DB()
+        {
+            try
+            {
+                //Open a connection & create a new dataset object
+                cnMain = new SqlConnection(strConn);
+                dsMain = new DataSet();
+            }
+            catch (SystemException e)
+            {
+                MessageBox.Show(e.Message, "Error");
+                return;
+            }
+        }
+
+        public void FillDataSet(string aSQLstring, string aTable)
+        {
+            //fills dataset fresh from the db for a specific table and with a specific Query
+            try
+            {
+                daMain = new SqlDataAdapter(aSQLstring, cnMain);
+                cnMain.Open();
+                //dsMain.Clear();   // need to have all the tables in the dataset
+                daMain.Fill(dsMain, aTable);
+                cnMain.Close();
+            }
+            catch (Exception errObj)
+            {
+                MessageBox.Show(errObj.Message + "  " + errObj.StackTrace);
+            }
+        }
+
+        protected bool UpdateDataSource(string sqlLocal, string table)
+        {
+            bool success;
+            try
+            {
+                //open the connection
+                cnMain.Open();
+                //***update the database table via the data adapter
+                daMain.Update(dsMain, table);
+                //---close the connection
+                cnMain.Close();
+                //refresh the dataset
+                FillDataSet(sqlLocal, table);
+                success = true;
+            }
+            catch (Exception errObj)
+            {
+                MessageBox.Show(errObj.Message + "  " + errObj.StackTrace);
+                success = false;
+            }
+            finally
+            {
+            }
+            return success;
+        }
+    }
+}
